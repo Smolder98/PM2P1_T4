b875936b354ea4f83ad74b37b53912d59e719902
Smolder98 <alicoescobar@gmail.com>
2022-06-19 23:26:39 -0600

    Add project files.

---
+++ PM2P1_T4/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using PM2P1_T4.Model;
+using PM2P1_T4.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM2P1_T4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            try
+            {
+                listImage.ItemsSource = await App.DBase.getListImagen();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private async void toolMenu1_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddPage());
+        }
+
+        private async void toolMenu2_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LocalPage());
+        }
+
+        private async void listImage_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = e.CurrentSelection.FirstOrDefault() as Imagen;
+
+            //await DisplayAlert("","Elemento seleccionado: " + Emple.nombre + " Fecha: "+ Emple.fechaIngreso, "OK");
+
+            ImagePage viewEmple = new ImagePage();
+            viewEmple.BindingContext = selected;
+
+            await Navigation.PushAsync(viewEmple);
+        }
+    }
+}
---
+++ PM2P1_T4/View/LocalPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using PM2P1_T4.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2P1_T4.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LocalPage : ContentPage
+    {
+        public LocalPage()
+        {
+            InitializeComponent();
+        }
+
+        protected  override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            string folderPath = "/storage/emulated/0/Android/data/com.companyname.pm2p1_t4/files/Pictures/MisFotos/";
+
+
+            try
+            {
+                string[] files = Directory.GetFiles(folderPath, "*.jpg");
+
+                List<Imagen> imagenes = new List<Imagen>();
+
+                Imagen temp = null;
+                foreach (var item in files)
+                {
+                    temp = new Imagen();
+
+                    temp.nombre = item.Remove(0, 82);
+                    temp.descripcion = item;
+
+                    imagenes.Add(temp);
+                }
+
+                listaImagen.ItemsSource = imagenes;
+            }
+            catch (Exception)
+            {
+               
+            }
+        }
+    }
+}
